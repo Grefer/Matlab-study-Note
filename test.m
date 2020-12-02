93b06ad901cfb6d81ca81d43fcd95eb319760dc6
Minevei <53202369+Minevei@users.noreply.github.com>
2020-12-02 18:04:31 +0800

    Update test.m

--- test.m
+++ test.m
@@ -2,4 +2,4 @@ begintime = '20190101';
 endtime = '20200101';
 code = '600029.SH';
 Exchange = 'SSE' ;
-[w_wsd_data,w_wsd_codes,w_wsd_fields,w_wsd_times,w_wsd_errorid] = w.wsd(code,'close',begintime,endtime,'Priceadj=B','tradingcalendar=Exchange ,'Days=Trading')  %获取收盘价（后复权）
+[w_wsd_data,w_wsd_codes,w_wsd_fields,w_wsd_times,w_wsd_errorid] = w.wsd(code,'close',begintime,endtime,'Priceadj=B',['tradingcalendar=',Exchange] ,'Days=Trading')  %获取收盘价（后复权）
