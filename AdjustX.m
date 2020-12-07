4a483e236690632cc66aff7ca7391982ac639937
Minevei <53202369+Minevei@users.noreply.github.com>
2020-12-07 17:55:21 +0800

    Update

---
+++ AdjustX.m
@@ -0,0 +1,23 @@
+%% 参数说明
+% St---t日股价
+% X---初始转股价
+% r---无风险利率
+% t---触发回售时点
+% T---到期日
+% sigma---波动率
+% I---t日应计利息
+% P---回售价格
+function K = AdjustX(St,X,r,t,T,sigma,I,P)
+    K = X;
+    d1 = (log(St/K) + (r + 0.5 * sigma^2)*(T-t))/(sigma * sqrt(T-t));
+    d2 = d1 - sigma*sqrt(T-t);
+    pd = makedist('Normal');
+    W = (St*cdf(pd,d1) - K*exp(-r*(T-t))*cdf(pd,d2))*100/K + (100+I)*exp(-r*(T-t));
+    while abs(W-P) > 0.001
+        if W > P
+            break
+        else
+            K = K - 0.01;
+        end
+    end
+end
--- sPath.m
+++ sPath.m
@@ -1,7 +1,7 @@
 function PricePath = sPath(S0,r,sigma,T,nStep,nPath)
-    dt = T/nStep;          %璁惧畾姝ラ暱
+    dt = T/nStep;          %设定步长
     PricePath = zeros(nPath ,1 + nStep);
-    PricePath(:,1) = S0;            %鍒濆鍖朣0
+    PricePath(:,1) = S0;            %初始化S0
     nudt = (r - 0.5 * sigma^2) * dt;
     sigmadt = sigma * sqrt(dt);
     for i = 1:nPath
@@ -9,15 +9,14 @@ function PricePath = sPath(S0,r,sigma,T,nStep,nPath)
             PricePath(i,j+1) = PricePath(i,j) * exp(nudt + sigmadt *randn);
         end
     end
-    figure
-    
-    for i = 1:nPath
-        plot(1:length(PricePath(i,:)),PricePath(i,:))
-        hold on;
-    end
-    hold off;
-    title('Simulation');
-    xlabel('Step')
-    ylabel('Stock Price')
+%     figure
+%     for i = 1:nPath
+%         plot(1:length(PricePath(i,:)),PricePath(i,:))
+%         hold on;
+%     end
+%     hold off;
+%     title('Simulation');
+%     xlabel('Step')
+%     ylabel('Stock Price')
 
 end
