92b82a2e1d4c66e7d40daabe06eb6459cdcc3990
Minevei <53202369+Minevei@users.noreply.github.com>
2020-12-11 15:20:40 +0800

    Update AdjustX.m

--- AdjustX.m
+++ AdjustX.m
@@ -9,10 +9,7 @@
 % P---回售价格
 function K = AdjustX(St,X,r,t,T,sigma,I,P)
     K = X;
-    d1 = (log(St/K) + (r + 0.5 * sigma^2)*(T-t))/(sigma * sqrt(T-t));
-    d2 = d1 - sigma*sqrt(T-t);
-    pd = makedist('Normal');
-    W = (St*cdf(pd,d1) - K*exp(-r*(T-t))*cdf(pd,d2))*100/K + (100+I)*exp(-r*(T-t));
+    W = blsprice(St,K,r,T-t,sigma)*100/K + (100+I)*exp(-r*(T-t));
     while abs(W-P) > 0.001
         if W > P
             break
