eb86fd51dd7a034d13a333fdd56ae5ab43ba8d4a
Grefer <53202369+Grefer@users.noreply.github.com>
2021-07-22 15:42:20 +0800

    update

--- volatility.m
+++ volatility.m
@@ -1,2 +1,2 @@
-Fvol_GARCH = Vol('CF109.CZC','20200713','20210712','CZCE')
+Fvol_GARCH = Vol('LH2109.DCE','20200721','20210721','DCE')
 disp(['Forecasted 1-year GARCH volatility = ' num2str(Fvol_GARCH*100,4) '%'])
